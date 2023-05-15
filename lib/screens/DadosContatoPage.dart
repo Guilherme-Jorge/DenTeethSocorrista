ca3074af856d5e04dc364aec7dba7140e4abc10e
Goldenn <joaopauloarrigo@gmail.com>
2023-05-15 18:23:37 -0300

    feat(app): jp-1 criei tela de cadastro da emergencia

--- lib/screens/DadosContatoPage.dart
+++ lib/screens/DadosContatoPage.dart
@@ -10,6 +10,11 @@ class DadosContatoPage extends StatefulWidget {
 }
 
 class _DadosContatoPageState extends State<DadosContatoPage> {
+  String _nome = "";
+  String _telefone = "";
+  String _motivo = "";
+
+  void pedirsocorro() {}
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +23,72 @@ class _DadosContatoPageState extends State<DadosContatoPage> {
         title: Text(widget.title),
       ),
       body: Center(
+          child: Padding(
+        padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Nome do socorrista *'),
+                  onChanged: (value) {
+                    setState(() {
+                      _nome = value;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Telefone de contato *'),
+                  onChanged: (value) {
+                    setState(() {
+                      _telefone = value;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Qual o motivo da emergência? (Opcional)'),
+                  onChanged: (value) {
+                    setState(() {
+                      _motivo = value;
+                    });
+                  },
+                )
+              ],
+            ),
+            Text('Nome: $_nome | Telefone: $_telefone | Motivo: $_motivo'),
+            TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.redAccent),
+                  padding: MaterialStateProperty.all(const EdgeInsets.symmetric(
+                      horizontal: 100, vertical: 14)),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Pedir socorro imediato',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                )),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
