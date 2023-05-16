15f2ba24aaeb99cc0ad39bf6206a9316b98dca16
Goldenn <joaopauloarrigo@gmail.com>
2023-05-16 10:14:30 -0300

    build(app): #3 configurei o flutter fire

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:denteeth/screens/InicioPage.dart';
 import 'package:denteeth/screens/DadosContatoPage.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
   runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
