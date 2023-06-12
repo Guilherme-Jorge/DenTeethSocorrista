089cbc50b66140e54947373f14bb755b3b2d180c
gugacs123 <decampossoaresgustavo@gmail.com>
2023-06-12 17:36:27 -0300

    Feat: funcionalidade para testar a conexão com a internet

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:denteeth/screens/ListaAprovados.dart';
 import 'package:denteeth/screens/Telefone.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
--- lib/screens/InicioPage.dart
+++ lib/screens/InicioPage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class InicioPage extends StatefulWidget {
-  const InicioPage({super.key, required this.title});
+  const InicioPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -18,37 +20,59 @@ class _InicioPageState extends State<InicioPage> {
         title: Text(widget.title, style: GoogleFonts.pacifico()),
       ),
       body: Center(
-          child: Padding(
-        padding: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 0),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Image(image: AssetImage('images/telaInicio.png')),
-          const SizedBox(height: 100),
-          SizedBox(
-            width: double.infinity,
-            child: TextButton(
-                style: ButtonStyle(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(image: AssetImage('images/telaInicio.png')),
+              const SizedBox(height: 100),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  style: ButtonStyle(
                     backgroundColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.blueAccent),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.fromLTRB(0, 6, 0, 6))),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/camera_boca');
-                },
-                child: Text('Emergência',
+                            (states) => Colors.blueAccent),
+                    padding:
+                    MaterialStateProperty.all(const EdgeInsets.fromLTRB(0, 6, 0, 6)),
+                  ),
+                  onPressed: () {
+                    internet_conexao();
+                  },
+                  child: Text(
+                    'Emergência',
                     style: GoogleFonts.pacifico(
-                        textStyle: const TextStyle(
-                            color: Colors.white, fontSize: 24)))),
+                      textStyle:
+                      const TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const SizedBox(
+                width: 300,
+                child: Text(
+                  '* Ao fazer o pedido de emergência você está de acordo com os nossos termos de serviços',
+                  style: TextStyle(fontSize: 12, color: Colors.black38),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 16),
-          const SizedBox(
-              width: 300,
-              child: Text(
-                '* Ao fazer o pedido de emergência você está de acordo com os nossos termos de serviços',
-                style: TextStyle(fontSize: 12, color: Colors.black38),
-                textAlign: TextAlign.center,
-              ))
-        ]),
-      )),
+        ),
+      ),
     );
+  }
+
+  void internet_conexao() async {
+    var conexao_resultado = await (Connectivity().checkConnectivity());
+    if (conexao_resultado == ConnectivityResult.none) {
+      Fluttertoast.showToast(
+        msg: 'Sem conexão com a internet',
+        toastLength: Toast.LENGTH_SHORT,
+      );
+    } else {
+      Navigator.pushNamed(context, '/camera_boca');
+    }
   }
 }
